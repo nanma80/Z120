5612eae398c0523f08583477eaf3e5a6fe727e16
nanma80 <mananself@gmail.com>
2014-02-27 22:17:43 -0800

    95 1156 42016

--- MPUlt/prog120.py
+++ MPUlt/prog120.py
@@ -3,8 +3,8 @@ import datetime
 
 filename_init = 'solved.log'
 # filename_current = 'trying.log'
-filename_current = 'Par19Trying.log'
-# filename_current = 'solving.log'
+# filename_current = 'Par19Trying.log'
+filename_current = 'solving.log'
 
 n_stickers_per_cell = 1 + 12 + 30 + 20 # 63
 n_cells = 120
